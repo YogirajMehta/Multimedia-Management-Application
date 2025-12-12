3814cae5b6d450f61562cf11ae92854cedeb1864
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 10:16:55 -0500

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-12-11-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #include "book.h"
 #include "settings.h"
 #include <sstream>
@@ -107,4 +100,5 @@ namespace seneca
         }
         return nullptr;
     }
+
 }
