65625326210e0c679f33c3a7ab1d66d5cebd7d04
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 10:15:55 -0500

    Update spellChecker.cpp

--- spellChecker.cpp
+++ spellChecker.cpp
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-12-11-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -57,4 +50,5 @@ namespace seneca
                 << ": " << m_replacementCounts[i] << " replacements\n";
         }
     }
+
 }
