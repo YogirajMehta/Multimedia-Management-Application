fe6f94d2176d5e6bc63c8a8340da92210e4dcec0
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 10:15:25 -0500

    Update tvShow.cpp

--- tvShow.cpp
+++ tvShow.cpp
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-12-11-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #include <iomanip>
 #include <algorithm>
 #include <numeric>
@@ -139,4 +132,5 @@ namespace seneca
         ss >> hours >> minutes >> seconds;
         return hours * 3600 + minutes * 60 + seconds;
     }
+
 }
