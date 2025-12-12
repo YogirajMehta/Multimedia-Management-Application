823b32e6e3ded05380bae5c8cbe6e3f15a122463
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 10:15:17 -0500

    Update tvShow.h

--- tvShow.h
+++ tvShow.h
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-12-11-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #ifndef SENECA_TVSHOW_H
 #define SENECA_TVSHOW_H
 #include <sstream>
@@ -124,5 +117,6 @@ namespace seneca
         targetShow->m_episodes.push_back(episode);
     }
 }
+
 
 #endif
