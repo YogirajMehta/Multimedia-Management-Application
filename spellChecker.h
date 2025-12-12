3cda5cfa5cd9e94db37d162cede5b2b20b84ea15
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 10:15:48 -0500

    Update spellChecker.h

--- spellChecker.h
+++ spellChecker.h
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-12-11-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #ifndef SENECA_SPELLCHECKER_H
 #define SENECA_SPELLCHECKER_H
 
@@ -32,5 +25,6 @@ namespace seneca
         void showStatistics(std::ostream& out) const;
     };
 }
+
 
 #endif 
