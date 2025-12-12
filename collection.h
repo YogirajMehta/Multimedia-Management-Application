e785e27da9de0e3e45bfbcf833102dc656aed6d3
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 10:16:23 -0500

    Update collection.h

--- collection.h
+++ collection.h
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-12-11-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #ifndef SENECA_COLLECTION_H
 #define SENECA_COLLECTION_H
 
@@ -46,5 +39,6 @@ namespace seneca
 
     std::ostream& operator<<(std::ostream& out, const Collection& coll);
 }
+
 
 #endif // SENECA_COLLECTION_H
